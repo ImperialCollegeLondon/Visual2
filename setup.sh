3ce5703412415528a200b2190fd2b8875a703931
tomcl <tomcl@users.noreply.github.com>
2018-08-10 10:58:04 +0100

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 git submodule update --init --recursive
 git submodule update --recursive --remote
+dotnet nuget locals all --clean
 dotnet restore src/Main/Main.fsproj
 dotnet restore src/Renderer/Renderer.fsproj
 dotnet restore src/Emulator/Emulator.fsproj
