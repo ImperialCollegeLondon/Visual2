43e844651056afc45b4147b2ad5c4f6e00f51748
tomcl <tomcl@users.noreply.github.com>
2018-08-10 10:55:04 +0100

    add nuget cache clear to setup.bat

--- setup.bat
+++ setup.bat
@@ -3,6 +3,7 @@ git submodule update --recursive --remote
 :: .paket\paket.bootstrapper.exe
 :: .paket\paket.exe install
 :: .paket\paket.exe update
+dotnet nuget locals all --clear 
 dotnet restore src\Main\Main.fsproj
 dotnet restore src\Renderer\Renderer.fsproj
 dotnet restore src\Emulator\Emulator.fsproj
