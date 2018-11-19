c5e160dbe19c6336eccc1839f52658ca66cdbf4c
tomcl <tomcl@users.noreply.github.com>
2018-11-19 00:35:41 +0000

    initial tb stuff

--- src/Renderer/Playground.fs
+++ src/Renderer/Playground.fs
@@ -11,16 +11,70 @@ open Fable.Core
 open Fable.Core.JsInterop
 open Fable.Import
 open Fable.Import.Browser
-
-
 open DP
+open Refs
+open EEExtensions
+open Helpers
+open CommonData
+
+type TbSpec =
+    | TbRegEquals of RName * uint32
+    | TbRegPointsTo of RName * uint32 list
+
+type TbInOut = TbIn | TbOut
+
+
+let parseTbLine lNum (lin:string) =
+    let (|RESOLVE|_|) lst =
+        let ops = 
+            String.concat " " lst
+            |> String.splitString [|","|] 
+            |> Array.map String.trim
+            |> Array.toList
+        let parseLiteral  = function | LITERALNUMB (lit,"") -> [lit] | _ -> []
+        let parseL = List.map parseLiteral ops
+        if List.exists ((=) []) parseL then None
+        else
+            List.concat parseL |> Some
+    let (|Defs|_|) words =
+        match words with
+        | [RegMatch (Ok rn) ; "IS" ; LITERALNUMB (lit,"")] -> TbRegEquals(rn,lit) |> Some
+        | RegMatch (Ok rn) :: "PTR" :: RESOLVE lits -> TbRegPointsTo(rn,lits) |> Some
+        | _ -> None
+    match lin.ToUpper() |> String.splitOnWhitespace |> Array.toList  with
+    | "IN" :: Defs tbSpec -> Ok (TbIn, tbSpec)
+    | "OUT" :: Defs tbSpec -> Ok (TbOut, tbSpec)
+    | _ -> Error (lNum,"Parse Error in testbench")
+
+
+
+let splitResult resL =
+    List.fold (fun (rl,el) -> function | Error e -> rl, e :: el | Ok r -> r :: rl, el) ([],[]) resL
+
+let decorateParseLine (i, pl) = ()
+
+let setupTest tl = ()
+
+let parseTestbench() =
+    if currentFileTabId < 0 then [ Error (-1,"No current file") ]
+    else
+        let testLines, testErrors = 
+            getCode currentFileTabId
+            |> String.splitString [|"\n"|]
+            |> Array.indexed
+            |> Array.map (fun (i,lin) -> parseTbLine i lin)
+            |> Array.toList
+            |> splitResult
+        if testErrors = [] then
+            testLines |> List.map Ok
+        else
+            testErrors |> List.map Error
+
+
+
 
 let check1() =
-    let x = 0xFF000000u
-    let y = uint32("-16777216")
-    printfn "numbs are: %x,%x,%x" x y (x-y)
-    match Helpers.parseNumberExpression Map.empty "-16777216" with
-    | Ok n -> printfn "Parsed as:%x" n
-    | _ -> printfn "no parse possible"
+    parseTestbench()
+
 
 
